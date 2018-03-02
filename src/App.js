616f3dba54ab7c6fdd54efb84804525bd5b437e9
julianleahy <julianleahynz@gmail.com>
2018-03-02 16:11:47 +1300

    add markup and style for modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Modal from './UI/Modal/Modal';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-
+                <Modal>
+                    Modal Content
+                </Modal>
             </div>
         );
     }
--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './Modal.css'
 
 const Modal = (props) => (
-        
+    <div className='Modal'>
+        <div className="ModalClose" onClick={props.click} >
+            <span className="ModalX">&times;</span>
+        </div>
+        {props.children}
+    </div>
 )
 
 export default Modal;
