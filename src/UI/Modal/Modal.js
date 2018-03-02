a96993ce1efc3536424c7d1f0f31c242e60be820
julianleahy <julianleahynz@gmail.com>
2018-03-02 16:25:26 +1300

    apply class to modal depending on props value

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Modal.css'
 
-const Modal = (props) => (
-    <div className='Modal'>
+const Modal = (props) => {
+    // hide modal when state is false
+    // change class to have modal appear from different directions [see css]
+    let modalClass = props.show ? ['Modal'] : ['Modal', 'ModalFromTop'];
+
+    return (
+        <div className={modalClass.join(' ')}>
+
         <div className="ModalClose" onClick={props.click} >
             <span className="ModalX">&times;</span>
         </div>
-        {props.children}
-    </div>
-)
+            {props.children}
+        </div>
+    )
+}
 
 export default Modal;
