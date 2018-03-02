8c050afcae06aeec6a7f55766ad559fcdc960017
julianleahy <julianleahynz@gmail.com>
2018-03-02 16:27:46 +1300

    style backdrop

--- src/UI/Backdrop/Backdrop.js
+++ src/UI/Backdrop/Backdrop.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Backdrop.css'
 
 const Backdrop = (props) => (
-        
+    // return component when showing modal
+    props.show ? <div className='Backdrop' onClick={props.click} ></div> : null       
 )
 
 export default Backdrop;
