a4e34b02bf08d9fa6ef61c56dc26d8db2671a3c4
julianleahy <julianleahynz@gmail.com>
2018-03-02 16:29:29 +1300

    add backdrop to app container and pass props to show and close

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from "react-redux";
 import Modal from './UI/Modal/Modal';
+import Backdrop from './UI/Backdrop/Backdrop';
 import * as Actions from './store/actions';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Backdrop show={this.props.showModal} click={this.props.toggleModal} />
                 <Modal
                     show={this.props.showModal}
                     click={this.props.toggleModal}>
